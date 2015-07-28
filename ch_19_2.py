7f428dac9dbbdd97f7d34626e20c3d0eac7f4e62
lgeorge <laurent.george@srand.fr>
2015-07-28 22:50:13 +0200

    adding graph base algorithms

---
+++ ch_19_2.py
@@ -0,0 +1,15 @@
+
+def _construct_maze(n=10, m=10, empty=50):
+    """
+    :param n: number of line
+    :param m: number of column
+    :param empty:
+    :return:
+    """
+    maze = [[0]*m]*n
+
+    return maze
+
+def find_path(maze):
+
+
